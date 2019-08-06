cee112c455a0a105f13aa72cce772f4a70d25eea
yukunkun <1163459047@qq.com>
2019-08-06 19:46:39 +0800

    update BriefActivity

--- app/src/main/java/com/matrix/yukun/matrix/video_module/play/BriefVersionActivity.java
+++ app/src/main/java/com/matrix/yukun/matrix/video_module/play/BriefVersionActivity.java
@@ -1,5 +1,6 @@
 package com.matrix.yukun.matrix.video_module.play;
 import com.matrix.yukun.matrix.R;
+import com.matrix.yukun.matrix.util.StatusBarUtil;
 import com.matrix.yukun.matrix.video_module.BaseActivity;
 import com.matrix.yukun.matrix.video_module.fragment.ToolFragment;
 
@@ -15,6 +16,10 @@ public class BriefVersionActivity extends BaseActivity {
 
     @Override
     public void initView() {
+        StatusBarUtil.setRootViewFitsSystemWindows(this, false);
+        //设置状态栏透明
+        StatusBarUtil.setTranslucentStatus(this);
+        StatusBarUtil.setStatusBarDarkTheme(this, true);
         getSupportFragmentManager().beginTransaction().add(R.id.fl, ToolFragment.getInstance()).commit();
     }
 }
