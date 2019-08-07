c95161e9e995ed80dbab24eb73da5719bd15ceb1
yukunkun <1163459047@qq.com>
2019-08-07 15:38:45 +0800

    Revert "update BriefActivity"
    This reverts commit cee112c455a0a105f13aa72cce772f4a70d25eea.

--- app/src/main/java/com/matrix/yukun/matrix/video_module/play/BriefVersionActivity.java
+++ app/src/main/java/com/matrix/yukun/matrix/video_module/play/BriefVersionActivity.java
@@ -1,6 +1,5 @@
 package com.matrix.yukun.matrix.video_module.play;
 import com.matrix.yukun.matrix.R;
-import com.matrix.yukun.matrix.util.StatusBarUtil;
 import com.matrix.yukun.matrix.video_module.BaseActivity;
 import com.matrix.yukun.matrix.video_module.fragment.ToolFragment;
 
@@ -16,10 +15,6 @@ public class BriefVersionActivity extends BaseActivity {
 
     @Override
     public void initView() {
-        StatusBarUtil.setRootViewFitsSystemWindows(this, false);
-        //设置状态栏透明
-        StatusBarUtil.setTranslucentStatus(this);
-        StatusBarUtil.setStatusBarDarkTheme(this, true);
         getSupportFragmentManager().beginTransaction().add(R.id.fl, ToolFragment.getInstance()).commit();
     }
 }
