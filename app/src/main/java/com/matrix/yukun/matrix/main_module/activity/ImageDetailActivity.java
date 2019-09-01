1dcaba5d5f82ab10d83f704a9e361ac7ef77cf77
yukunkun <1163459047@qq.com>
2019-09-01 11:18:22 +0800

    update

--- app/src/main/java/com/matrix/yukun/matrix/main_module/activity/ImageDetailActivity.java
+++ app/src/main/java/com/matrix/yukun/matrix/main_module/activity/ImageDetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.ImageView;
@@ -14,6 +15,10 @@ import com.bumptech.glide.Glide;
 //import com.bumptech.glide.load.resource.drawable.GlideDrawable;
 //import com.bumptech.glide.request.animation.GlideAnimation;
 //import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.matrix.yukun.matrix.BaseActivity;
 import com.matrix.yukun.matrix.R;
 import com.matrix.yukun.matrix.R2;
@@ -62,30 +67,18 @@ public class ImageDetailActivity extends BaseActivity {
 //        StatusBarUtil.setStatusBarDarkTheme(this, true);
         downloadurl = getIntent().getStringExtra("url");
         mIsGif = getIntent().getBooleanExtra("isGif",false);
+        Glide.with(this).load(downloadurl).listener(new RequestListener<Drawable>() {
+            @Override
+            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                return false;
+            }
 
-//        Glide.with(this).load(downloadurl).into(new GlideDrawableImageViewTarget(mPhotoview){
-//            @Override
-//            public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> animation) {
-//                super.onResourceReady(resource, animation);
-//                mProgressBar.setVisibility(View.GONE);
-//            }
-//
-//            @Override
-//            public void onLoadFailed(Exception e, Drawable errorDrawable) {
-//                super.onLoadFailed(e, errorDrawable);
-//                ToastUtils.showToast( "图片加载失败");
-//                mProgressBar.setVisibility(View.GONE);
-//            }
-//        });
-
-
-        if (mIsGif) {
-            Glide.with(ImageDetailActivity.this).asGif().load(downloadurl).into(mIvImage);
-            mPhotoview.setVisibility(View.GONE);
-            mProgressBar.setVisibility(View.GONE);
-        } else {
-            Glide.with(this).load(downloadurl).into(mPhotoview);
-        }
+            @Override
+            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                mProgressBar.setVisibility(View.GONE);
+                return false;
+            }
+        }).into(mPhotoview);
     }
 
     @OnClick({R2.id.iv_more, R2.id.iv_back})
